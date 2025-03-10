61fa24fda4ecfb2f02793ccae8314c0e327c0b84
vicrdguez <contact@vicrodriguez.dev>
2025-03-10 13:33:35 +0400

    fix issues with tf-azure template

--- src/terraform/azure/flake.nix
+++ src/terraform/azure/flake.nix
@@ -2,8 +2,8 @@
   description = "Flake to work with Terraform resources an Azure";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.utils = "github:numtide/flake-utils";
-  inputs.dsh.url = "github:numtide:devshell";
+  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.dsh.url = "github:numtide/devshell";
 
   outputs = { dsh, nixpkgs, utils }:
     (utils.lib.eachDefaultSystem (system:
